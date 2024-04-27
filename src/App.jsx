71138f4e0cabd75e16e5bbcae7ea4cd48a98470b
rabbyananto <rabby.ananto@gmail.com>
2024-04-27 15:53:08 +0200

    Qr code generator : functionality react

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Qrcode from "qrcode";
+import { useState } from "react";
+
+const App = () => {
+  const [url, setUrl] = useState("");
+  const [qrcode, setQrcode] = useState("");
+
+  const GenerateQrCode = () => {
+    Qrcode.toDataURL(
+      url,
+      {
+        width: 800,
+        margin: 2,
+      },
+      (err, url) => {
+        if (err) return console.error(err);
+        console.log(url);
+        setQrcode(url);
+      }
+    );
+  };
+  return (
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        placeholder="a input"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)}
+      />
+      <button onClick={GenerateQrCode}>Generate</button>
+      {qrcode && (
+        <>
+          <img src={qrcode} alt="Qrcode image" />
+          <a href={qrcode} download="qrcode.png">
+            Download
+          </a>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
